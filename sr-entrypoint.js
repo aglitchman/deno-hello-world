f8ef918b510e014dbaec5c8b99dcd028379dcf5e
Artsiom Trubchyk <aglitchman@gmail.com>
2021-05-30 13:43:30 +0300

    Create sr-entrypoint.js

---
+++ sr-entrypoint.js
@@ -0,0 +1,37 @@
+
+function error(status, err) {
+  return new Response(err, {
+    status: status,
+    headers: {
+      "content-type": "text/plain; charset=UTF-8",
+    },
+  });
+}
+
+async function handleRequest(request) {
+  const { pathname, searchParams } = new URL(request.url);
+
+  if (pathname == "/") {
+    return new Response(
+      `<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.4/tailwind.min.css" rel="stylesheet">
+      <div class="px-3 py-3">
+        <h2>Hello, World!</h2>
+      </div>`,
+      {
+        headers: {
+          "content-type": "text/html; charset=UTF-8",
+        },
+      }
+    );
+  }
+
+  return new Response(`Hello, World! #2`, {
+    headers: {
+      "content-type": "text/html; charset=UTF-8",
+    },
+  });
+}
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
